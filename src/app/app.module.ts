5044db692340355ed6b1681d99211bd882bafa36
Jose marcos <72453138+SakakibaraKun@users.noreply.github.com>
2023-05-16 09:40:44 -0300

    Componente HOME criado

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -25,11 +25,13 @@ import { MatIconModule } from '@angular/material/icon';
 import { MatListModule } from '@angular/material/list';
 import { MatCardModule } from '@angular/material/card';
 import { AppRoutingModule } from './app-routing.module';
+import { HomeComponent } from './components/home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavComponent
+    NavComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
