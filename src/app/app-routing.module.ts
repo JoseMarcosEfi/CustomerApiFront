b26f6cbbf31ef338b0f0da0e63d36af752d46dd3
Jose marcos <72453138+SakakibaraKun@users.noreply.github.com>
2023-06-21 17:11:20 -0300

    Rota para componente customer-create

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,6 +5,7 @@ import { HomeComponent } from './components/home/home.component';
 import { TecnicoListComponent } from './components/tecnico/tecnico-list/tecnico-list.component';
 import { LoginComponent } from './components/login/login.component';
 import { CustomerListComponent } from './components/customer/customer-list/customer-list.component';
+import { CustomerCreateComponent } from './components/customer/customer-create/customer-create.component';
 
 const routes: Routes = [
   {path: 'login', component:LoginComponent},
@@ -12,7 +13,8 @@ const routes: Routes = [
     path: '',  component: NavComponent, children: [
       { path: 'home', component: HomeComponent },
       { path: 'tecnicos', component: TecnicoListComponent },
-      { path: 'customer', component: CustomerListComponent}
+      { path: 'customer', component: CustomerListComponent },
+      { path: 'customer/create', component: CustomerCreateComponent}
     ]
   }
 ];
