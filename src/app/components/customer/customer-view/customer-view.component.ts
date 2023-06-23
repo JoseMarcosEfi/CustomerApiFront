891e2808b5ee0231d47a44532f56cf9d39e1260a
Jose marcos <72453138+SakakibaraKun@users.noreply.github.com>
2023-06-23 13:48:25 -0300

    Customer-view concluido

--- src/app/components/customer/customer-view/customer-view.component.ts
+++ src/app/components/customer/customer-view/customer-view.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Customer } from 'src/app/models/customer';
+import { CustomerService } from 'src/app/services/customer.service';
 
 @Component({
   selector: 'app-customer-view',
@@ -6,5 +9,28 @@ import { Component } from '@angular/core';
   styleUrls: ['./customer-view.component.css']
 })
 export class CustomerViewComponent {
+customer: Customer = {
+    id: '',
+    name: '',
+    cpf: '',
+    email: '',
+    password: ''
+  }
+  constructor(
+    private service: CustomerService,
+    private route: ActivatedRoute,
+  ) { }
+
+  ngOnInit(): void{
+    this.customer.id = this.route.snapshot.paramMap.get('id');
+    this.findById();
+  }
+  
+  findById(): void{
+    this.service.findById(this.customer.id).subscribe(resposta => {
+      //resposta.perfis = []
+      this.customer = resposta;
+    })
+  }
 
 }
